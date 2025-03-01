ce2e9475b5498f6ab6c1402a1edfda9d563f12b2
MohamedMbarouk <mmm1070@nyu.edu>
2025-03-02 01:28:26 +0400

    Learnt about one-line if statements and quick in-line ways of generating data. Current process is computationally exhausting. Learnt about itertools will use them next time

--- Problem 43: Sub-string Divisibility [DL5%].py
+++ Problem 43: Sub-string Divisibility [DL5%].py
@@ -20,14 +20,28 @@ import numpy as np
 def pandigital_generator():
     '''
     Description:
-        Generates a list of all 3,265,920 possible 10-digit pandigital numbers out of 9,000,000,000 possible 10-digit numbers. 
+        Generates a list of all 3,265,920 (10!-9! = 9*9!) possible 10-digit pandigital numbers out of 9,000,000,000 (9*10^9)possible 10-digit numbers. 
     Args:
         None. 
     Returns:
-        pandigital_list (ndarray) : Array of size 3,265,920 that contains all existing pandigital numbers
+        pandigital_array (ndarray) : Array of size 3,265,920 that contains all existing pandigital numbers
     '''
-    #Defining an array of our factors
-    factor_array = np.array([3,5])
+    #Defining the number of pandigital numbers 
+    number_of_pandigitals = 3265920 
+    #Creating the list that will store our discovered pandigital and be converteed to array at the end
+    pandigital_list = []
+
+    #Rather than geneate each number, it makes more sense to check that a given 10-digit number is pandigital by checking that each digit only appears once
+    for num in range(int(10e9),int(10e10)):
+        #Converting a number into a numpy array of all its digits by converting the subject number into a string, generating a list which holds each string and creating a new list that converts each element of the string list into an int
+        num_np = np.array([int(element) for element in (list(str(num)))])
+        #Check if the array has only unique elements and if so, add it to our pandigital_list
+        if len(np.unique(num_np))==10: pandigital_list.append(num)
+    print(pandigital_list)
+    print(len(pandigital_list))
+
+
+pandigital_generator()
 
 #Defining a function to evaluate whether a given 10-digit number is 0 to 9 pandigital
 def pandigital_checker(value):

6d0c46864ad2bb309fbd35d06917a7dd61fa5add
MohamedMbarouk <mmm1070@nyu.edu>
2025-03-04 23:45:10 +0400

    Produced pandigitals with itertools.permutations, few tweaks needed to generate proper numbers git push

--- Problem 43: Sub-string Divisibility [DL5%].py
+++ Problem 43: Sub-string Divisibility [DL5%].py
@@ -27,12 +27,16 @@ def pandigital_generator():
     Returns:
         pandigital_array (ndarray) : Array of size 3,265,920 that contains all existing pandigital numbers
     '''
-    #Defining the number of pandigital numbers 
-    number_of_pandigitals = 3265920 
+    #Creating a list digits to use to create pandigitals
     list_of_digits = [0,1,2,3,4,5,6,7,8,9]
-    #Creating the list that will store our discovered pandigital and be converteed to array at the end
-    pandigital_list = list(itertools.permutations(list_of_digits,10))
-    print(pandigital_list)
+    #Creating the list of pandigitals using permutations but only including the value if the starting digit is not 0
+    pandigital_list = [dig for dig in itertools.permutations(list_of_digits,10) if dig[0]!=0]
+    #Combining the digits from the itertools (that come as a comma seperated list) into one value and resaving it to my pandigital list
+    #pandigital_list = (int(''.join(str(digit) for digit in value)) for value in pandigital_list)
+    my_list = [(1, 0, 2, 3, 4, 5, 6, 7, 8, 9), (1, 0, 2, 3, 4, 5, 6, 7, 9, 8), (1, 0, 2, 3, 4, 5, 6, 8, 7, 9)]
+    #my_list_proper = (int(''.join(str(digit) for digit in value)) for value in my_list)
+    print(d for d in my_list)
+
 
     
 

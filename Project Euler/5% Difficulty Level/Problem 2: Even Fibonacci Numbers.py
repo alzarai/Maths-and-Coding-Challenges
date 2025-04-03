0faa70fe1cfd30a9bad237b95235809d37f4c942
MohamedMbarouk <mmm1070@nyu.edu>
2025-04-04 00:44:15 +0300

    Started a new challenge and refined file structure of old challenges

--- Project Euler/5% Difficulty Level/Problem 2: Even Fibonacci Numbers.py
+++ Project Euler/5% Difficulty Level/Problem 2: Even Fibonacci Numbers.py
@@ -9,12 +9,22 @@ By considering the terms in the Fibonacci sequence whose values do not exceed fo
 import numpy as np
 
 def even_fibos():
+    '''
+    Description:
+        Returns the sum of all even-valued Fibonacci numbers under four million
+    Args:
+        None
+    Returns:
+        even_fibo_sum (unt): The sum of the list that contained all even Fibonacci numbers under four million
+    '''
+    #Defining the start of the Fibonacci Sequence
     fibo_list = [1,2]
     looper = 0
     while (fibo_list[-1] < 4e6):
+        #Defining the next term of the sequence and appending to our list
         fibo_list.append(fibo_list[looper] + fibo_list[looper+1])
         looper+=1
-    #Even fibonacci numbers
+    #Using list comprehension to filter only the even Fibonacci numbers
     even_fibo_list = [num for num in fibo_list if (num%2)==0]
     #Converting to a numpy array for easy summing
     even_fibo_sum = np.sum(np.array(even_fibo_list))
--- Project Euler/5% Difficulty Level/Problem 6: Sum Square Difference.py
+++ Project Euler/5% Difficulty Level/Problem 6: Sum Square Difference.py
@@ -9,11 +9,22 @@ Find the difference between the sum of the squares of the first one hundred natu
 import numpy as np
 
 def sum_square_difference(count):
+    '''
+    Description:
+        Returns the difference between sum of squares of natural numbers and the square of the sum
+    Args:
+        count (int): The range (i.e. max value) that we want to compute for
+    Returns:
+        diff (int): The difference (i.e. value required) but in absolute terms because I'm lazy
+    '''
+
     #Creating a list of natural numbers required
     natural_numbers_array = np.linspace(1,count,count)
+    #Creating sum of squares and square of sums list
     sum_of_squares = np.sum([num**2 for num in natural_numbers_array])
     square_of_sums = (np.sum(natural_numbers_array))**2
-    diff = sum_of_squares - square_of_sums
-    return(np.abs(diff))
+    #Calculating the difference 
+    diff = np.abs(sum_of_squares - square_of_sums)
+    return(diff)
 
 print(sum_square_difference(100))

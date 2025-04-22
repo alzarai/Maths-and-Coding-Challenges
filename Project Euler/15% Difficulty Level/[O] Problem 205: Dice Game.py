56f6476d89475007d41497b4486e309a6e3dfdf7
MohamedMbarouk <mmm1070@nyu.edu>
2025-04-23 00:02:18 +0400

    Starting the dice challenge

--- Project Euler/15% Difficulty Level/[O] Problem 205: Dice Game.py
+++ Project Euler/15% Difficulty Level/[O] Problem 205: Dice Game.py
@@ -7,14 +7,25 @@ PROBLEM STATEMENT
 https://projecteuler.net/problem=205
 '''
 import itertools
+import numpy as np
 
+'''
+Lets start this problem simpler. Let us define user1 with 2 dice that can choose values [2,5] and user2 with 2 dice of values [3,4]
+We know that user1 values = [(2,2),(2,5),(5,2),(5,5)] and user2 values = [(3,3,(3,4),(4,3),(4,4)]
+'''
+user_1=(5,7)
+user_2=(1,2)
+u1 = list(itertools.product(user_1, repeat=len(user_1)))
+u1_sum = list(map(lambda x: sum(x),u1))
+u2 = list(itertools.product(user_2, repeat=len(user_2)))
+u2_sum = list(map(lambda x: sum(x),u2))
+print(u1_sum,u2_sum)
 
-pyramidal_values = [1,2,3,4]
-cubic = [1,2,3,4,5,6]
+totals = (len(u1_sum) + len(u2_sum))
+u1_greater = 0
+for u1_val in u1_sum:
+    for u2_val in u2_sum:
+        if u1_val > u2_val:
+            u1_greater+=1
 
-g1 = [1,2,3,4]
-g2 = [3,4]
-
-total_vals = list(itertools.combinations((g1),2))
-itertools.combinations()
-print(total_vals)
+print(u1_greater/totals)
